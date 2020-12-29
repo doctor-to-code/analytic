3f67a8d2d4607926ebe533a2ac1d7556dabdb387
Peter Yang <fgdeeerry@gmail.com>
2020-12-29 16:03:46 +0800

    analytic text

--- analytic.py
+++ analytic.py
@@ -4,10 +4,17 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data. append(line)
 		count += 1
-		if count % 10000 == 0: #count/1000餘數是0的意思
+		if count % 10000 == 0: #count/1000餘數是0的意思:每一千筆記錄一次意思
 			print(len(data))
 print(len(data))
 print(data[5])
 print('----------------------------------')
 print(data[15])
 print('----------------------------------')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print(sum_len)
+k = sum_len/len(data)
+print(k)
