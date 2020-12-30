dae55a45f230f5035ce5c07e43d119e2eb001af9
Peter Yang <fgdeeerry@gmail.com>
2020-12-30 11:54:08 +0800

    fast analytic

--- analytic.py
+++ analytic.py
@@ -1,22 +1,13 @@
 data = []
-count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data. append(line)
-		count += 1
-		if count % 10000 == 0: #count/1000餘數是0的意思:每一千筆記錄一次意思
-			print(len(data))
+print(len(data))
+good = [g for g in data if 'good' in g]
+print(len(good))
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '留言長度小於100')
-print(new[10])
+fuck = [d for d in data if 'fuck' in d]
+print(len(fuck))
 
-good = []
-for g in data:
-	if 'good' in g:
-		good.append(g)
-print('一共有', len(good), '留言包含good')
-print(good[10])
+love = [l for l in data if 'love' in l]
+print(len(love))
